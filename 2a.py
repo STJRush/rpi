3d233ada950d707eb22f2db90a33434d1a36494f
Daniel Murray <dcuish@gmail.com>
2016-01-13 21:57:32 +0000

    INSULTRON
    Made this task more fun

--- 2a.py
+++ 2a.py
@@ -1,7 +1,16 @@
 """
-This example will ask the user their name and say hello
+This example will ask the user for some input and then insult them.
 """
-
-name = raw_input("What is your name? ")
-print "Hello, %s." % name
+thingy=input("Waiting for slow human input...still waiting...")                                 #type something
+print("You're a " +thingy + "!!!")                                                              #Say it back
+print("  ____   ___  ____   ___ _____ _____ __        _____ _   _ _                     ")
+print(" |  _ \ / _ \| __ ) / _ \_   _|__  / \ \      / /_ _| \ | | |                    ")
+print(" | |_) | | | |  _ \| | | || |   / /   \ \ /\ / / | ||  \| | |                    ")
+print(" |  _ <| |_| | |_) | |_| || |  / /_    \ V  V /  | || |\  |_|                    ")
+print(" |_|_\_\\___/|____/_\___/ |_|_/____|   _\_/\_/ _|___|_| \_(_) _   _ ____         ")
+print("  _| || |_| |   / _ \| |    / __ \| | | | | | |  \/  |  / \  | \ | / ___|        ")
+print(" |_  ..  _| |  | | | | |   / / _` | |_| | | | | |\/| | / _ \ |  \| \___ \        ")
+print(" |_      _| |__| |_| | |__| | (_| |  _  | |_| | |  | |/ ___ \| |\  |___) |       ")
+print("   |_||_| |_____\___/|_____\ \__,_|_| |_|\___/|_|  |_/_/   \_\_| \_|____/        ")
+print("                            \____/                                               ")
       
