d75f10ac9a1429daca7fabf642165584080ab9b8
MrDMurray <daniel.murray@stjosephsrush.ie>
2018-02-15 14:12:47 +0000

    fixed timing bug limiting code to 100 readings

--- webwatersensor/graph_distSimple.py
+++ webwatersensor/graph_distSimple.py
@@ -32,7 +32,7 @@ x = []  #placeholders for the graph later on
 y = []
 
 
-def distcheck():
+def distcheck():  #function to measure distance
   count = 0
    
   while True:
@@ -43,27 +43,23 @@ def distcheck():
 
     while GPIO.input(ECHO)==0:  #measure some times
       pulse_start = time()
-
+      
     while GPIO.input(ECHO)==1:
       pulse_end = time()
 
+    sleep(0.01)
+    
     pulse_duration = pulse_end - pulse_start  #does the maths
-
-
-    # The next values are "global", meaning the distance values from here can be used outside of this function.
-    # "global" changes the value of distance everywhere. Without making these global, the graph later
-    # on would keep reading 0 even though distance in this function would be updating. The change to distance
-    # would never leave the bounds of this function.
 
     distance = pulse_duration * 17150   #distance = time * speed of sound
 
-    global distance; distance = round(distance, 2)       #rounds distance values off to 2 places
+    distance = round(distance, 2)       #rounds distance values off to 2 places
 
    # print ("Distance:",distance,"cm")                    #adds units to make it look neater
     return(distance)
 
 
-def send_mail_alert():
+def send_mail_alert():  #function to send email alert
   to = 'dcuish@gmail.com'
   """
   gmail_user = input("Enter full email eg. RPITime@gmail.com")
@@ -73,7 +69,6 @@ def send_mail_alert():
   smtpserver.ehlo()
   smtpserver.starttls()
   smtpserver.login(gmail_user, gmail_password)
-  #today = datetime.date.today()
 
 
   my_msg=("WARNING! Liquid Level limits exceeded. Disistance at " + str(mode) + "cm")
@@ -94,9 +89,9 @@ def send_mail_alert():
 
 print("Welcome to liquid level meter")
 print("Turn on email alert?")
-choice=input("y/n?")
+email_choice=input("y/n?")
 
-if choice == "y":
+if email_choice == "y": #choose to turn email alerts on or off
   mailAlert = 1 #on
   print("Mail alerts on")
   limit=input("Set a limit in cm to trigger an email alert eg. 5 ")
@@ -106,10 +101,15 @@ if choice == "y":
   gmail_password = getpass.getpass(prompt="Type in your password")
 
   
-elif choice == "n":
+elif email_choice == "n":
   print("That's cool.")
   mailAlert = 0 #off
 
+print("Do you want to graph the results?")  #choose to turn graphing on or off
+graph_choice=input("y/n?")
+
+datacount = 0 #for no graph mode, a measurement count is handy to know how long
+#the sensor has been running. This delcares a variable to count measurments.
 
 while True:
 
@@ -118,36 +118,45 @@ while True:
     number_of_readings = 50  #how many readings to take and find the mode of
     
     for pizza in range(number_of_readings):  #takes 10 readings
-        distance=distcheck()  #checks distance
+        distance = distcheck()  #checks distance
         readings_list.append(int(distance)) #puts the 10 values in a list
 
-    print(readings_list) #prints the list
+    print(readings_list) #prints the list and gets the mode (most common value)
     mode = max(set(readings_list), key=readings_list.count)
     print("The mode of " + str(number_of_readings) + " values is " + str(mode))
 
-    if mode <10 and mailAlert == 1:  #sends email if dist <10cm
+    if mode <10 and mailAlert == 1:  #sends email if mode is <10cm
       send_mail_alert()
       sleep(5)
-      mailAlert = 0 #stops more than one email alert
+      mailAlert = 0 #stops more than one email alert being sent
       
       print("DANGEROUS LEVELS. EMAIL ALERT SENT>>>>>>>>")
       
-    #writes this data to a table (a .csv file)
+    #writes this data to a table (a .csv file), this is needed for the graph
     with open("cpu_dists.csv", "a") as log:   # "a" means append to the end
         log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(mode)))
+
+
+    if graph_choice == "y":
+
+
+      #the code below makes the graph and controlls it's settings
         
-    days = time()/(60*60*24) #converts seconds to days
-    
-    y.append(mode)   #puts the distance values on the y-axis
-    x.append(days)    #puts the distance values on the x-axis
-    plt.clf()
-    plt.scatter(x,y)    #chooses the scatter plot chart type
-    plt.plot(x,y)
-    plt.suptitle("Distance over time")
-    plt.xlabel("Time (Days)")
-    plt.ylabel("Distance (cm)")
-    plt.draw()          #draws the graph (or if an a loop, updates the graph)
+      days = time()/(60*60*24) #graph shows seconds but we want days 
+      
+      y.append(mode)   #puts the distance values on the y-axis
+      x.append(days)    #puts the distance values on the x-axis
+      plt.clf()
+      plt.scatter(x,y)    #chooses the scatter plot chart type
+      plt.plot(x,y)
+      plt.suptitle("Distance over time")  #graph title
+      plt.xlabel("Time (Days)")
+      plt.ylabel("Distance (cm)")
+      plt.draw()          #draws the graph (or if an a loop, updates the graph)
 
-    sleep(0.01)         #Waits between measurements. This could be made longer to save power.
+      sleep(0.01)         #Waits between measurements. This could be made longer to save power.
 
+    elif graph_choice == "n":
+      datacount=datacount+1
+      print("Measurement " + str(datacount))
 
